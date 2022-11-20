1e3d0ccb6f326642b0a11e41ab5a1d50d9815dcd
Danyon <danyonstitt@gmail.com>
2022-11-20 15:39:54 +1300

    added functionality to color, erase, and reset buttons

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-// Define the number of columns and rows
-let rows = prompt("Choose the grid size")
-let columns = rows;
-let squares = rows * columns;
+// Setting an empty variable for the color
+let color = "black";
 
 // Create the variable that selects the container
 const container = document.querySelector(".container");
 
-// For the number of columns specified
+function makeGrid() {
+    // Define the number of columns and rows
+    let rows = prompt("Choose the grid size");
 
-function makeRows(rows, columns) {
+    while(rows > 100) {
+        alert("Choose a number from 1 to 100!")
+        rows = prompt("Choose the grid size");
+    }
+    if(rows === null) {
+        rows = 50;
+    };
+
+    // Set properties
     container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", columns);
-    for (i = 0; i < (rows*columns); i++) {
+    container.style.setProperty("--grid-cols", rows);
+
+    // create each square
+    for (i = 0; i < (rows*rows); i++) {
         let cell = document.createElement("div");
         cell.className = "cell";
         container.appendChild(cell);
     };
 };
 
-makeRows(rows, columns);
+makeGrid();
 
-function changeColor(e) {
-    e.target.style.backgroundColor = "black";
-};
-
+// Selecting 
 const cells = document.querySelectorAll(".cell");
 cells.forEach(cell => {
     cell.addEventListener("mouseover", changeColor);
 });
+
+function changeColor(e) {
+    e.target.style.backgroundColor = color;
+};
+
+// add event listener to each of the buttons
+const black = document.querySelector(".black");
+const rainbow = document.querySelector(".rainbow");
+const erase = document.querySelector(".erase");
+const reset = document.querySelector(".reset");
+
+black.addEventListener("click", () => {color = "black"});
+rainbow.addEventListener("click", () => {color = "red"});
+erase.addEventListener("click", () => {color = "white"});
+reset.addEventListener("click", () => {resetGrid()});
+
+function resetGrid() {
+    cells.forEach(cell => {
+        cell.style.backgroundColor = "white";
+    });
+};
+
+
+
+
+
